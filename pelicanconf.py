0d787ef62adc0de7972749590937fa65ea6bad2f
mousa smady <mousa@mousas-MacBook-Pro.local>
2019-06-13 10:39:32 -0400

    Add all my files

---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Mousa Smady'
+SITENAME = 'blog'
+SITEURL = 'https://mousam96.github.io/blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
